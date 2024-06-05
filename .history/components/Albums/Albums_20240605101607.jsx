59236856cb92acf1b06c8a43d6646270209c2120
DevLaukey <63729244+DevLaukey@users.noreply.github.com>
2024-06-05 10:21:31 +0300

    feat(album homepage): added fetch call and displayed data

---
+++ .history/components/Albums/Albums_20240605101607.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Albums() {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const usersData = fetch(
+    "https://jsonplaceholder.typicode.com/todos/",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => console.error(error));
+
+  if (usersData.length) {
+    return (
+      <div>
+        {usersData.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </div>
+    );
+  }
+
+  return <div>Albums</div>;
+}
+
+export default Albums;
